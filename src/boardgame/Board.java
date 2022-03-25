f5158524a45ad57912d880f0d9e0036c75d52584
Vitor Silva <svitoryago@hotmail.com>
2022-03-24 21:32:39 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -77,4 +77,20 @@ public class Board {
 	public boolean thereIsAPiece(Position position) {
 		return piece(position) != null;
 	}
+	
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		
+		if(piece(position) == null) {
+			return null;
+		}
+		
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		
+		return aux;
+	}
 }
