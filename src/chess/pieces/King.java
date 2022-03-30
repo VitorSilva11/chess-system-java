1b1e4648353ec5f945d8abffda9c3b1d29c39ba5
Vitor Silva <svitoryago@hotmail.com>
2022-03-29 22:53:55 -0300

    Possible moves of a piece

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -17,4 +17,13 @@ public class King extends ChessPiece{
 		return "K";
 	}
 
+
+	@Override
+	public boolean[][] possibleMoves() {
+		
+		boolean[][] mat = new boolean[getBoard().getRow()][getBoard().getColumn()];
+		
+		return mat;
+	}
+
 }
